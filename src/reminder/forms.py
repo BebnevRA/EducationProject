89209e58d7e4213c479d5dec36f39435849bc3dd
etroks <bebnevra>
2025-03-12 00:26:54 +0300

    fix forms

--- src/reminder/forms.py
+++ src/reminder/forms.py
@@ -1,16 +1,17 @@
 from django import forms
 
 
-class NoteEditForm(forms.Form):
+class NoteForm(forms.Form):
     note_text = forms.CharField(max_length=500)
+    author = forms.CharField(max_length=500, required=False)
     note_notify_start = forms.DateTimeField(initial=None, required=False)
     note_notify_gap = forms.DurationField(initial=None, required=False)
     checkbox_notify_start = forms.BooleanField(required=False)
     checkbox_notify_gap = forms.BooleanField(required=False)
 
-class NoteAddForm(forms.Form):
-    note_text = forms.CharField(max_length=500, required=True)
-    author = forms.CharField(max_length=500, required=False)
-    note_create_date = forms.DateTimeField(initial=None, required=False)
-    note_notify_start = forms.DateTimeField(initial=None, required=False)
-    note_notify_gap = forms.DurationField(initial=None, required=False)
+    def clean_note_notify_gap(self):
+        note_notify_gap = self.cleaned_data['note_notify_gap']
+        note_notify_start = self.cleaned_data['note_notify_start']
+        if note_notify_gap and (note_notify_start is None):
+            raise forms.ValidationError('Для создания промежутка - задайте время первого уведомления')
+        return note_notify_gap
